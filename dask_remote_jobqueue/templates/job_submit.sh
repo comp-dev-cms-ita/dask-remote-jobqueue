158cb9f96e1f00a34b11487b5bed6b23ade00723
ciangottini <ciangottini@pg.infn.it>
2021-09-24 17:09:05 +0200

    job submit env

--- dask_remote_jobqueue/templates/job_submit.sh
+++ dask_remote_jobqueue/templates/job_submit.sh
@@ -4,4 +4,13 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
+export _condor_AUTH_SSL_CLIENT_CAFILE={{ htc_ca }}
+export _condor_TOOL_DEBUG={{ htc_debug }}
+export _condor_COLLECTOR_HOST={{ htc_collector }}
+export _condor_SCHEDD_HOST={{ htc_schedd_host }}
+export _condor_SCHEDD_NAME={{ htc_schedd_name }}
+export _condor_SCITOKENS_FILE={{ htc_scitoken_file }}
+export _condor_SEC_DEFAULT_AUTHENTICATION_METHODS={{ htc_sec_method}}
+
+
 condor_submit -spool $@
