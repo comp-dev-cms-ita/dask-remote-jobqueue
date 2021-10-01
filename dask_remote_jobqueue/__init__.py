d85d8af7222224c4d4c69a7176fcaca4d175ea97
MircoT <MircoT@users.noreply.github.com>
2021-10-01 10:53:48 +0200

    Fix logger file

--- dask_remote_jobqueue/__init__.py
+++ dask_remote_jobqueue/__init__.py
@@ -3,4 +3,8 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
+import logging
+
 from .dask_remote_jobqueue import *
+
+logging.basicConfig(filename="/var/log/dask_remote_jobqueue.log", level=logging.DEBUG)
