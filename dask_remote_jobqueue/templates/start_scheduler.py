9d084aa82c40f64b4e6b181daf46cb3219b6bcb1
ciangottini <ciangottini@pg.infn.it>
2021-10-17 09:37:29 +0200

    strip \n from classad

--- dask_remote_jobqueue/templates/start_scheduler.py
+++ dask_remote_jobqueue/templates/start_scheduler.py
@@ -70,7 +70,7 @@ with open(machineAd_file) as f:
     lines = f.readlines()
     for l in lines:
         if l.startswith("SiteName"):
-            site = l.split("= ")[1]
+            site = l.split("= ")[1].strip("\n")
             break
 
 logger.debug("SiteName is: %s" % site)
