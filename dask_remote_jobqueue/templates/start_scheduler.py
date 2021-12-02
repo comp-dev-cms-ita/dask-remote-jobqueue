0326e6dbd38736629052c3b22e6446760dc7e041
MircoT <MircoT@users.noreply.github.com>
2021-12-02 10:12:05 +0100

    Add scratch local directory

--- dask_remote_jobqueue/templates/start_scheduler.py
+++ dask_remote_jobqueue/templates/start_scheduler.py
@@ -28,6 +28,7 @@ class MyHTCondorJob(HTCondorJob):
             *args,
             **kwargs,
             python="source /cvmfs/cms.dodas.infn.it/miniconda3/etc/profile.d/conda.sh; conda activate cms-dodas; source /cvmfs/cms.dodas.infn.it/miniconda3/envs/cms-dodas/bin/thisroot.sh; python3",
+            local_directory="./scratch",
         )
 
 
