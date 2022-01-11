c5f403b16759b98fd6c8412ecba83a6d002d9c82
MircoT <MircoT@users.noreply.github.com>
2022-01-11 14:34:50 +0100

    Add tini to scheduler launch

--- dask_remote_jobqueue/templates/scheduler.sh
+++ dask_remote_jobqueue/templates/scheduler.sh
@@ -9,22 +9,26 @@ chmod +x job_rm.sh
 
 # Configure oidc-agent for user token management
 echo "eval \`oidc-keychain\`" >>~/.bashrc
-eval $(oidc-keychain)
-oidc-gen dodas --issuer $IAM_SERVER \
-    --client-id $IAM_CLIENT_ID \
-    --client-secret $IAM_CLIENT_SECRET \
-    --rt $REFRESH_TOKEN \
+eval "$(oidc-keychain)"
+oidc-gen dodas --issuer "$IAM_SERVER" \
+    --client-id "$IAM_CLIENT_ID" \
+    --client-secret "$IAM_CLIENT_SECRET" \
+    --rt "$REFRESH_TOKEN" \
     --confirm-yes \
     --scope "openid profile email" \
     --redirect-uri http://localhost:8843 \
     --pw-cmd "echo \"DUMMY PWD\""
 
 while true; do
-    oidc-token dodas --time 1200 > token
+    oidc-token dodas --time 1200 >token
     sleep 600
 done &
 
 source /cvmfs/cms.dodas.infn.it/miniconda3/etc/profile.d/conda.sh
 conda activate cms-dodas
 
-python3 start_scheduler.py
+if command -V tini &>/dev/null; then
+    tini -s python3 -- start_scheduler.py
+else
+    python3 start_scheduler.py
+fi
