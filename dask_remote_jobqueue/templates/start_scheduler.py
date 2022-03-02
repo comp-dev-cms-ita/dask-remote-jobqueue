7eb29c3db2d076553c76259d64b27a4215160bce
MircoT <MircoT@users.noreply.github.com>
2022-03-02 09:47:51 +0100

    Switch to new env for root

--- dask_remote_jobqueue/templates/start_scheduler.py
+++ dask_remote_jobqueue/templates/start_scheduler.py
@@ -44,7 +44,7 @@ class MyHTCondorJob(HTCondorJob):
             *args,
             **kwargs,
             death_timeout=60 * 5,  # 5min
-            python="source /cvmfs/cms.dodas.infn.it/miniconda3/etc/profile.d/conda.sh; conda activate cms-dodas; source /cvmfs/cms.dodas.infn.it/miniconda3/envs/cms-dodas/bin/thisroot.sh; python3",
+            python="source /cvmfs/cms.dodas.infn.it/root-cc7/bin/thisroot.sh; python3",
         )
 
 
