2fe5205a28d259c3160bddbb7608b3f17e58a499
Diego Ciangottini <diego.ciangottini@pg.infn.it>
2023-03-22 10:47:58 +0100

    try fixing #60

--- dask_remote_jobqueue/templates/scheduler.sh
+++ dask_remote_jobqueue/templates/scheduler.sh
@@ -20,9 +20,6 @@ while true; do
     sleep 72000
 done &
 
-source /cvmfs/cms.dodas.infn.it/miniconda3/bin/activate
-conda activate af-test
-
 if command -V tini &>/dev/null; then
     tini -s python3 -- start_scheduler.py
 else
